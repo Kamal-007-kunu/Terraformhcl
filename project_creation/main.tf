261de76318ba8db7acb5059ce547d7356572f546
Kamal Juyal <Kamal Juyal>
2025-08-03 17:54:53 +0530

    asbuilt design task

--- project_creation/main.tf
+++ project_creation/main.tf
@@ -14,10 +14,11 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "app_server" {
+  count         = 9   
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
 
   tags = {
-    Name = "learn-terraform"
+    Name = "learn-terraform-${count.index}"
   }
 }
